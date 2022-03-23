3f2a06f3259f6f79b5008ab1dd8b6a2f6a7f7a11
Bernardo Poetsch <89049723+nadupoetsch@users.noreply.github.com>
2022-03-23 02:57:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var num = Math.floor(Math.random() * 3)
+
+switch(num){
+  case 0:
+    alert('Mande imediatamente os documentos por SEDEX AR até dia 04/09!') ;
+    break;
+  case 1:
+    alert('Mande no final do semeste!') ;
+    break;
+  case 2:
+    alert('Não mande!') ;
+    break;
+}
+
+alert('') ;
+alert('teste') ;
